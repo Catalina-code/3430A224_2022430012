afbb7ae65d16f2dd18269e9beb64f4aae19a4ea6
Catita Figueroa <catafigueroa@MacBook-Air-de-Catita.local>
2024-08-27 10:06:36 -0400

    Añadir archivo paciente_arreglo.cpp

---
+++ paciente_arreglo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string> // Necesario para usar std::string
+
+using namespace std;
+
+// Definición de la estructura Paciente
+struct Paciente {
+    string nombre;
+    int edad;
+    double peso;
+    double altura;
+};
+
+int main() {
+    
+
+}
